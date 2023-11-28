3ba19dd02defb823e93e1cc82f0dd3c76fe42f16
Leandro Sousa <151054241+sousalmdev@users.noreply.github.com>
2023-11-28 11:36:29 -0300

    Update estudo.js
    Shortened version

--- estudo.js
+++ estudo.js
@@ -1,31 +1,23 @@
 let nome="Goku"
-let vitórias=200;
-let derrotas=6;
+let vitórias=9;
+let derrotas=0;
 let media=vitórias-derrotas;
-
+//
 function saldoRank(){
-    
-    if(media<10){
-        return `Ferro`
-
-}else if(media>10 && media<=20) {
-    return `Bronze`
-
-} else if(media>20 && media<=50) {
-    return `Prata`
-
-} else if(media>50 && media<=80) {
-    return `Ouro`
-
-} else if(media>80 && media<=90) {
-    return `Diamante`
-
-} else if(media>90 && media<=100) {
-    return `Lendário`
-
-} else if(media>100) {
-    return `Imortal`
-
+    switch (true) {
+        case media<10 : return `Ferro`;
+        break;
+        case media>10 && media<=20:return `Bronze`;
+        break;
+        case media>20 && media<=50: return `Prata`;
+        break;
+        case media>50 && media<=80:return `Ouro`;
+        break;
+        case media>80 && media<=90: return `Diamante`;
+        break;
+        case media>90 && media<=100 :return `Lendário`;
+        break;
+        case media>100: return `Imortal`;
+    }   
 }
-};
-console.log("O Héroi "+ nome+ " tem saldo de:" + media + " e está no ranking:" + saldoRank());
+console.log("O Héroi "+ nome+ " tem saldo de: " + media + " e está no ranking: "+saldoRank());
